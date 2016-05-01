a22df7e23690ca40e5c2fd8cd4bf418d8eb67a1e
Nikita Marchant <nikita.marchant@gmail.com>
2016-05-01 11:03:06 +0200

    Wrap the line processing in a try/except so that it doesn't stop all the processing

--- scraper/merger.py
+++ scraper/merger.py
@@ -157,19 +157,22 @@ def timestamps_to_model(timestamps, line, way):
     }
 
 def save_day(arg):
-    row_list, line, way = arg
-    dates, positions = zip(*row_list)
+    try:
+        row_list, line, way = arg
+        dates, positions = zip(*row_list)
 
-    trajects = trajects_from_bool(positions)
-    trajects = map(skip_terminus, trajects)
-    trajects = map(reduce_traject, trajects)
+        trajects = trajects_from_bool(positions)
+        trajects = map(skip_terminus, trajects)
+        trajects = map(reduce_traject, trajects)
 
-    timestamps = (traject_to_timestamps(t, dates) for t in trajects)
-    models = [timestamps_to_model(t, line, way) for t in timestamps]
+        timestamps = (traject_to_timestamps(t, dates) for t in trajects)
+        models = [timestamps_to_model(t, line, way) for t in timestamps]
 
-    if len(models) > 0:
-        with db.atomic():
-            Traject.insert_many(models).execute()
+        if len(models) > 0:
+            with db.atomic():
+                Traject.insert_many(models).execute()
+    except Exception as e:
+        print(line, way, e)
 
 
 if __name__ == '__main__':
@@ -180,13 +183,15 @@ if __name__ == '__main__':
     from multiprocessing import Pool
 
     p = Pool(4)
-    LINES = [1,2,3,4,5,6,7,94,25]
+    # LINES = [1,2,3,4,5,6,7,94,25]
     routes = [(line, 1) for line in LINES] + [(line, 2) for line in LINES]
+    routes = [(12, 1), (12, 2), (13, 1), (13, 2), (14, 1), (14, 2), (15, 1), (15, 2), (19, 1), (19, 2), (20, 1), (20, 2), (21, 1), (21, 2), (22, 1), (22, 2), (25, 2), (27, 1), (27, 2), (28, 1), (28, 2), (29, 1), (29, 2), (32, 1), (32, 2), (34, 1), (34, 2), (36, 1), (36, 2), (38, 1), (38, 2), (39, 1), (39, 2), (41, 1), (41, 2), (42, 1), (42, 2), (43, 1), (43, 2), (44, 1), (44, 2), (45, 1), (45, 2), (46, 1), (46, 2), (47, 1), (47, 2), (48, 1), (48, 2), (49, 1), (49, 2), (50, 1), (50, 2), (51, 1), (51, 2), (53, 1), (53, 2), (54, 1), (54, 2), (55, 1), (55, 2), (57, 1), (57, 2), (58, 1), (58, 2), (59, 1), (59, 2), (60, 1), (60, 2), (61, 1), (61, 2), (62, 1), (62, 2), (63, 1), (63, 2), (64, 1), (64, 2), (65, 1), (65, 2), (66, 1), (66, 2), (69, 1), (69, 2), (71, 1), (71, 2), (72, 1), (72, 2), (75, 1), (75, 2), (76, 1), (76, 2), (77, 1), (77, 2), (78, 1), (78, 2), (79, 1), (79, 2), (80, 1), (80, 2), (81, 1), (81, 2), (82, 1), (82, 2), (84, 1), (84, 2), (86, 1), (86, 2), (87, 1), (87, 2), (88, 1), (88, 2), (89, 1), (89, 2), (92, 1), (92, 2), (93, 1), (93, 2), (97, 1), (97, 2), (98, 1), (98, 2)]
     for line, way in routes:
         print("Line", line, "way", way)
         dates, positions = get_data_from_db(line, way)
         iterable = zip(dates, positions)
 
+        # TODO : décaler de 3 heures pour ne pas couper les bus qui roulment après minuit
         groups = [(list(v), line, way) for k, v in groupby(iterable, lambda x: x[0].date())]
         p.map_async(save_day, groups)
 
